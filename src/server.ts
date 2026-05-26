af71de49cd8a6bad573b00cf8311c5f7b4ff633e
AHMED <ahmedtexting@gmail.com>
2026-05-26 22:19:20 +0600

    db integrated

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
+import { env } from "node:process";
+import { Pool } from "pg"
 const app: Application = express()
 const port = 5000
-app.use(express.json())
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }))
+const pool = new Pool({
+    connectionString: env.databaseUrl
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
@@ -9,8 +17,13 @@ app.get('/', (req: Request, res: Response) => {
         "author": "Ahmed"
     })
 })
-app.post('/', async (req:Request, res: Response) => {
-    console.log(req.body);
+app.post('/', async (req: Request, res: Response) => {
+    // console.log(req.body);
+    const body = req.body;
+    res.status(201).json({
+        message: "created successfully",
+        data: body
+    })
 })
 
 
