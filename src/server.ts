c5b477cdcb3461936f84bb06ef25b0743850ec98
AHMED <ahmedtexting@gmail.com>
2026-05-26 23:23:02 +0600

    db connected

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,41 @@
 import express, { type Application, type Request, type Response } from "express"
-import { env } from "node:process";
+import dotenv from "dotenv";
 import { Pool } from "pg"
 const app: Application = express()
 const port = 5000
+dotenv.config();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }))
+const databaseUrl = process.env.DATABASE_URL
 const pool = new Pool({
-    connectionString: env.databaseUrl
-})
+    connectionString: databaseUrl
+});
+
+const initDB = async () => {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY, 
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+        )`);
+        console.log('DATABASE Connected Successfully!!');
+
+    } catch (error) {
+        console.log(error);
+
+    }
+};
+initDB();
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
