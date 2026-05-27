24d0dbad905cc8d1f6dba26980eecb5a36b4d733
AHMED <ahmedtexting@gmail.com>
2026-05-27 12:40:01 +0600

    create user with unique to avoid duplicate

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const initDB = async () => {
             CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY, 
             name VARCHAR(20),
-            email VARCHAR(20) NOT NULL,
+            email VARCHAR(20) UNIQUE NOT NULL,
             password VARCHAR(20) NOT NULL,
             is_active BOOLEAN DEFAULT true,
             age INT,
@@ -47,15 +47,21 @@ app.post('/', async (req: Request, res: Response) => {
     // console.log(req.body);
     const { name, email, password, age } = req.body;
 
-    const result = await pool.query(`
+    try {
+        const result = await pool.query(`
         INSERT INTO users(name,email,password,age) VALUES($1,$2,$3,$4) RETURNING *
         `, [name, email, password, age])
-    res.status(201).json({
-        message: "created successfully",
-        data: result.rows[0]
-            
-        
-    })
+        res.status(201).json({
+            message: "User created successfully!!",
+            data: result.rows[0]
+        })
+
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message,
+            error: error
+        })
+    }
 })
 
 
