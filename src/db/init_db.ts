e205cae10b0ae289212ba2392625ed226115557e
AHMED <ahmedtexting@gmail.com>
2026-06-08 21:22:11 +0600

    add: Managing the user profile with SQL reference

--- src/db/init_db.ts
+++ src/db/init_db.ts
@@ -21,8 +21,22 @@ export const initDB = async () => {
             updated_at TIMESTAMP DEFAULT NOW()
 
         )`);
-        console.log('DATABASE Connected Successfully!!');
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS profiles(
+        id SERIAL PRIMARY KEY,
+        user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+        bio TEXT,
+        address TEXT,
+        phone VARCHAR(15),
+        gender VARCHAR(10),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
 
+        )    
+            `)
+
+
+        console.log('DATABASE Connected Successfully!!');
     } catch (error) {
         console.log(error);
 
