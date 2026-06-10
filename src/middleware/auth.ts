2d1feb6dff6817ea92cee9bf1a3bdb2d6f035144
AHMED <ahmedtexting@gmail.com>
2026-06-10 21:59:00 +0600

    Add: Auth Middleware Validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import config from "../config/config";
+import { pool } from "../db/init_db";
 
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -8,10 +11,27 @@ const auth = () => {
         if (!token) {
             res.status(401).json({
                 success: false,
-                message: "Unauthorized Access !!",
-                data: []
+                message: "Unauthorized Access !!!"
             })
+        }
 
+        const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload
+        const userData = await pool.query(`
+            SELECT * FROM users WHERE email=$1
+            `, [decoded.email])
+
+        const user = userData.rows[0]
+        if (userData.rows.length === 0) {
+            res.status(401).json({
+                success: false,
+                message: "User Not Found !!!"
+            })
+        }
+        if (!user.is_active) {
+            res.status(403).json({
+                success: false,
+                message: "Forbidden !!!"
+            })
         }
 
         next()
